f2ce5797320f851eed3ed029db52ba7f559cf5f8
Chan Nguyen <channn3@fpt.edu.vn>
2024-06-24 10:23:07 +0700

    n

--- app/src/main/java/com/demo/demo1/MainActivity.kt
+++ app/src/main/java/com/demo/demo1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.demo.demo1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.d("TAG", "onCreate: ")
     }
 }
