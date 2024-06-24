a72d4c0cd2fdc2078e7949033169c3e07c9ab548
Chan Nguyen <channn3@fpt.edu.vn>
2024-06-24 10:23:56 +0700

    hello

--- app/src/main/java/com/demo/demo1/MainActivity.kt
+++ app/src/main/java/com/demo/demo1/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +14,6 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         Log.d("TAG", "onCreate: ")
+        Log.d("TAG", "onCreate: hello")
     }
 }
